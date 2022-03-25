61629dbc3817a150dc50a94048cc777a39c909bf
shahriar <[200~shahriar.sj330@gmail.com~>
2022-03-25 23:16:20 +0600

    my code

---
+++ view/demo.js
@@ -0,0 +1,39 @@
+// let n='shahh'
+// console.log(n.toUpperCase());
+// console.log(n.toLocaleUpperCase());
+// let m=" SJAJ"
+// console.log(m.toUpperCase());
+// console.log(m.toLocaleLowerCase());
+// let v='shahriar';
+// console.log(v.startsWith('shah'));
+// console.log(v.endsWith('riar'));
+// let o='   sja   jj';
+// console.log(o.padStart());
+// console.log(o.trimStart())
+// console.log(o.trim())
+// let a=10,n=20;
+// console.log(a+n);
+// let a=10,b=20;
+// a=100,b=300;
+// let result=a+b;
+// console.log(result);
+// let c=10,d=30;
+// let result1=c-d;
+// console.log(result1);
+// let e=100,f=200;
+// let result2=e*f;
+// console.log(result2);
+// let g=100,h=2
+// let result3=g/h;
+// console.log(result3);
+// let i=10,j=20;
+// let result4=i%j;
+// console.log(result4);
+
+
+
+
+
+
+
+
